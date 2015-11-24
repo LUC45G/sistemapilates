eb857de26cb501920c542d46b4f4c54be2da8e2d
nahuelmorata <nahuelmorata@gmail.com>
2015-11-24 09:37:15 -0500

    Incorporacion del SqliteModel.py

---
+++ models/SqliteModel.py
@@ -0,0 +1,19 @@
+# -*- coding: UTF-8 -*-
+import sqlite3
+from config import db
+
+class SqliteClass():
+  def __init__(self):
+    self.con = sqlite3.connect(db)
+    self.cursor = self.con.cursor()
+    
+  def EjecutarConsulta(self, sql):
+    self.cursor.execute(sql)
+    self.con.commit()
+    
+  def Consulta(self, sql):
+    self.cursor.execute(sql)
+    return self.cursor
+  
+  def Cerrar(self):
+    self.con.close()
