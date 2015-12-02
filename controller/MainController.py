5215b91f3a18ccb70dbb20143a048a04344ade75
Nahuel Morata <nahuelmorata@gmail.com>
2015-12-01 22:14:19 -0300

    Incoporacion del archivo AlumnoModel.py y uso

--- controller/MainController.py
+++ controller/MainController.py
@@ -1,10 +1,7 @@
-from models.SqliteModel import SqliteClass
+#from models.SqliteModel import SqliteClass
 
 class MainController():
   
   def __init__(self):
-    self.db = SqliteClass()
-    self.db.EjecutarConsulta("CREATE TABLE IF NOT EXISTS alumnos(\
-                            id INT PRIMARY KEY NOT NULL,\
-                            nombre TEXT NOT NULL)")
-    self.db.Cerrar()
+    """self.db = SqliteClass()
+    self.db.Cerrar()"""
---
+++ models/AlumnoModel.py
@@ -0,0 +1,18 @@
+from SqliteModel import SqliteClass
+
+class Alumno():
+
+	def __init__(self):
+	    self.db = SqliteClass()
+	    self.db.EjecutarConsulta("CREATE TABLE IF NOT EXISTS alumnos(\
+                            id INT PRIMARY KEY NOT NULL,\
+                            nombre TEXT NOT NULL,\
+                            apellido TEXT NOT NULL,\
+                            dni INT NOT NULL)")
+
+	def GetName(self, id):
+		nombre = self.db.Consulta("SELECT nombre FROM alumnos WHERE id = " + id)
+		return nombre[0]
+
+	def SetName(self, id, nombre):
+		self.db.EjecutarConsulta("UPDATE alumnos SET nombre = '" + nombre + "' WHERE id = " + id)
