422efd714800d24b59ba82b9d7ac392f61f3ebe3
lucaas_gg <lucaas_gg@hotmail.com>
2016-02-06 15:47:18 -0500

    .

--- models/AlumnoModel.py
+++ models/AlumnoModel.py
@@ -1,5 +1,5 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
+# -*- coding: UTF-8 -*-
+
 from SqliteModel import SqliteClass
 
 class Alumno():
@@ -7,42 +7,21 @@ class Alumno():
 	def __init__(self):
 	    self.db = SqliteClass()
 	    self.db.EjecutarConsulta("CREATE TABLE IF NOT EXISTS alumnos(\
-                            id INT PRIMARY KEY NOT NULL,\
+                            id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,\
                             nombre TEXT NOT NULL,\
                             apellido TEXT NOT NULL,\
                             dni INT NOT NULL, \
 														turnos TEXT NOT NULL)")
 
-	def GetName(self, id):
-		nombre = self.db.Consulta("SELECT nombre FROM alumnos WHERE id = " + id)
-		return nombre[0]
+#	def GetName(self, id):
+#		nombre = self.db.Consulta("SELECT nombre FROM alumnos WHERE id = " + id)
+#		return nombre[0]
+#
+#	def SetName(self, id, nombre):
+#		self.db.EjecutarConsulta("UPDATE alumnos SET nombre = '" + nombre + "' WHERE id = " + id)
 
-	def SetName(self, id, nombre):
-=======
-=======
->>>>>>> 76d08c364069a6e3580fa84ea57e6aa1a73708ec
-# -*- coding: UTF-8 -*-
-
-from .SqliteModel import SqliteClass
-
-class AlumnoClass():
-
-	def __init__(self):
-	    self.db = SqliteClass()
-	    self.db.EjecutarConsulta("CREATE TABLE IF NOT EXISTS alumnos(\
-                            id INT PRIMARY KEY NOT NULL,\
-                            nombre TEXT NOT NULL,\
-                            apellido TEXT NOT NULL,\
-                            dni INT NOT NULL, \
-                            turnos TEXT NOT NULL)")
-
-	def GetName(self, id):
-		nombre = self.db.Consulta("SELECT nombre FROM alumnos WHERE id = " + id)
-		return nombre[0]
-
-	def SetName(self, id, nombre):
-<<<<<<< HEAD
->>>>>>> 76d08c364069a6e3580fa84ea57e6aa1a73708ec
-=======
->>>>>>> 76d08c364069a6e3580fa84ea57e6aa1a73708ec
-		self.db.EjecutarConsulta("UPDATE alumnos SET nombre = '" + nombre + "' WHERE id = " + id)
+	def AddAlumno(self, nombre, apellido, dni):
+		self.db.EjecutarConsulta("INSERT INTO alumnos (nombre, apellido, dni) VALUES ('" + nombre + "', '" + apellido + "', " + dni +")")
+		
+	def DelAlumno(self, id):
+		self.db.EjecutarConsulta("DELETE FROM alumnos WHERE id = " + id)
--- models/ProfesorModel.py
+++ models/ProfesorModel.py
@@ -1,3 +1,5 @@
+# -*- coding: UTF-8 -*-
+
 from SqliteModel import SqliteClass
 
 class Profesor():
@@ -5,16 +7,23 @@ class Profesor():
   def __init__(self):
       self.db = SqliteClass()
       self.db.EjecutarConsulta("CREATE TABLE IF NOT EXISTS profesores(\
-                                id INT PRIMARY KEY NOT NULL,\
+                                id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,\
                                 nombre TEXT NOT NULL, \
                                 apellido TEXT NOT NULL, \
                                 dni INT NOT NULL, \
                                 turnos TEXT NOT NULL)")
       
-  def GetName(self, id):
-    nombre = self.db.Consulta("SELECT nombre FROM profesores WHERE id = " + id )
-    return nombre[0]
-  
-  def SetName(self, id, nombre):
-    self.db.EjecutarConsulta("UPDATE profesores SET nombre '" + nombre + "' WHERE id = " + id)
+#  def GetName(self, id):
+#    nombre = self.db.Consulta("SELECT nombre FROM profesores WHERE id = " + id )
+#    return nombre[0]
+#  
+#  def SetName(self, id, nombre):
+#    self.db.EjecutarConsulta("UPDATE profesores SET nombre = '" + nombre + "' WHERE id = " + id)
     
+  def AddProfesor(self, nombre, apellido, dni):
+    self.db.EjecutarConsulta("INSERT INTO profesores (nombre, apellido, dni) VALUES ('" + nombre + "', '" + apellido + "', " + dni + ")")
+    
+  def DelProfesor(self, id):
+    self.db.EjecutarConsulta("DELETE FROM profesores WHERE id = " + id)
+    
+  
